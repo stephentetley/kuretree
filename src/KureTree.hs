006a7d393fd3e5747f3a3623658632792eb51db3
stephentetley <stephen.tetley@gmail.com>
2018-03-21 22:47:14 +0000

    Adding skeleton project files.

---
+++ src/KureTree.hs
@@ -0,0 +1,23 @@
+{-# OPTIONS -Wall #-}
+
+--------------------------------------------------------------------------------
+-- |
+-- Module      :  KureTree
+-- Copyright   :  (c) Stephen Tetley 2018
+-- License     :  BSD3
+--
+-- Maintainer  :  stephen.tetley@gmail.com
+-- Stability   :  unstable
+-- Portability :  GHC
+--
+-- Placeholder...
+--
+--------------------------------------------------------------------------------
+
+
+module KureTree where
+
+import Language.KURE                    -- package: kure
+
+import Text.JSON                        -- package: json
+
